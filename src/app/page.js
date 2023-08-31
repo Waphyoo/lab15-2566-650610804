69fe2065e276a62c184566d9fd53fbf9d0da8f55
Waphyoo <nonny2246@gmail.com>
2023-08-31 16:25:58 +0700

    okok

--- src/app/page.js
+++ src/app/page.js
@@ -30,6 +30,10 @@ const schema = z
     lastName: z
       .string()
       .min(3, { message: "Last name must have at least 3 characters" }),
+    password: z
+      .string()
+      .min(6, { message: "Password must contain at least 6 characters" })
+      .max(12, { message: "Password must not exceed 12 characters" }),
     email: z.string().email({ message: "Invalid email format" }),
     plan: z.enum(["funrun", "mini", "half", "full"], {
       errorMap: (issue, ctx) => ({ message: "Please select a plan" }),
@@ -44,7 +48,7 @@ const schema = z
     }),
     hasCoupon: z.boolean(),
     coupon: z.string(),
-    password: z.string(),
+
     confirmPassword: z.string(),
   })
   .refine(
@@ -62,6 +66,20 @@ const schema = z
     {
       message: "Invalid coupon code",
       path: ["coupon"],
+    }
+  )
+  .refine(
+    (data) => {
+      // if user does not tick "I have coupon", then it's ok
+
+      // if user tick "I have coupon" and fill correct code, then it's ok too
+      if (data.confirmPassword === data.password) return true;
+      // ticking "I have coupon" but fill wrong coupon code, show error
+      return false;
+    },
+    {
+      message: "Password does not math",
+      path: ["confirmPassword"],
     }
   );
 
@@ -90,11 +108,14 @@ export default function Home() {
     //TIP : get value of currently filled form with variable "form.values"
 
     if (form.values.plan === "funrun") price = 500;
+    if (form.values.plan === "mini") price = 800;
+    if (form.values.plan === "half") price = 1200;
+    if (form.values.plan === "full") price = 1500;
     //check the rest plans by yourself
     //TIP : check /src/app/libs/runningPlans.js
 
     //check discount here
-
+    if (form.values.hasCoupon && form.values.coupon === "CMU2023") price *= 0.7;
     return price;
   };
 
@@ -155,7 +176,7 @@ export default function Home() {
 
             <Text fw="bold">
               {" "}
-              Total Price : {computePrice().toLocaleString()} BATH
+              Total Price : {computePrice().toLocaleString()} bath
             </Text>
 
             <Divider variant="dashed" />
@@ -175,7 +196,11 @@ export default function Home() {
           </Stack>
         </form>
 
-        <Footer year={2023} fullName="Chayanin Suatap" studentId="650610560" />
+        <Footer
+          year={2023}
+          fullName="Watunyoo Phanapaisarnsakul"
+          studentId="650610804"
+        />
       </Container>
 
       <TermsAndCondsModal opened={opened} close={close} />
